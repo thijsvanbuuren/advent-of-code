1a9ef72732900b5e23610504dda7b2e138348b57
Thijs van Buuren <thijsvanbuurenisc@nl.ibm.com>
2021-12-10 12:01:03 +0100

    2021: Day 10

--- src/main/java/advent/_2021/Day10.kt
+++ src/main/java/advent/_2021/Day10.kt
@@ -2,14 +2,70 @@ package advent._2021
 
 import advent.util.ADay
 import advent.util.Day
+import org.slf4j.LoggerFactory
+import java.util.*
 
-@ADay(2021, 10, "day0")
-class Day0 : Day {
+@ADay(2021, 10, "day10")
+class Day10 : Day {
+    val logger = LoggerFactory.getLogger("Day 10")
+
+    val openers = listOf('(', '[', '{', '<')
+    val closers = listOf(')', ']', '}', '>')
     override fun doPart1(input: List<String>): Any {
-        return -1
+        return input.mapNotNull {
+            checkLine(it)
+        }.sumOf {
+            when (it) {
+                ')' -> 3
+                ']' -> 57
+                '}' -> 1197
+                '>' -> 25137
+                else -> 0
+            } as Int
+        }
+    }
+
+    private fun checkLine(line: String): Char? {
+        val stack = Stack<Char>()
+        line.forEach {
+            if (openers.contains(it)) {
+                stack.push(it)
+            } else {
+                val shouldBe = closers[openers.indexOf(stack.pop())]
+                if (it != shouldBe) {
+                    logger.debug("Error:\n$line\nExpected $shouldBe is $it")
+                    return it
+                }
+            }
+        }
+        return null
     }
 
     override fun doPart2(input: List<String>): Any {
-        return -1
+        return input.filter {
+            checkLine(it) == null
+        }.map {
+            var first = false
+
+            fixLine(it).map { closers.indexOf(it) + 1L }.reduce { a, b ->
+                val total: Long = a * 5L + b
+                logger.debug(" $a * 5 + $b = $total")
+                total
+            }
+        }.sorted().let { it[it.size / 2] }
+    }
+
+    private fun fixLine(line: String): List<Char> {
+        val stack = Stack<Char>()
+        line.forEach {
+            if (openers.contains(it)) {
+                stack.push(it)
+            } else {
+                stack.pop()
+            }
+        }
+        val result = stack.toList().reversed().map { closers[openers.indexOf(it)] }
+        logger.debug("Missing for :\n$line\nAre $result")
+        return result
     }
 }
